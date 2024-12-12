1014636efef208f974260fae4e30350da48dcefd
Christian Eichert <c@zp1.net>
2024-12-12 07:18:31 +0100

    modified:   tests/ApacheTest.php

--- tests/ApacheTest.php
+++ tests/ApacheTest.php
@@ -84,11 +84,11 @@ EOD;
         // // $this->assertCount(3, $result);
 
         // Check structure of the first row
-        // // $this->assertArrayHasKey('KEY1', $result[1]);
-        // // $this->assertArrayHasKey('KEY2', $result[1]);
-        // // $this->assertArrayHasKey('KEYn', $result[1]);
+        $this->assertArrayHasKey('KEY1', $result[0]);
+        $this->assertArrayHasKey('KEY2', $result[0]);
+        $this->assertArrayHasKey('KEYn', $result[0]);
 
-        // // Check values of each row
+        // Check values of each row
         $this->assertEquals('VALUEa1', $result[0]['KEY1']);
         $this->assertEquals('VALUEa2', $result[0]['KEY2']);
         $this->assertEquals('VALUEan', $result[0]['KEYn']);
